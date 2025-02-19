4b1df5289618f4e269e57a2e35a311cf73e4dbf2
Shajahan A I <shajahaniqbal57@gmail.com>
2025-02-19 12:46:28 +0530

    Page Routing in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
-    <>
-      <Home></Home>
-    </>
+    <div>
+      <NavBar></NavBar>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
-
 
 export default App;
